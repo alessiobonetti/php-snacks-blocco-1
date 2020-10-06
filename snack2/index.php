6b3ef35234eab364e1978bf4e2784515e4d2aba4
Alessio Bonetti <alesine87@gmail.com>
2020-10-06 11:03:32 +0200

    snack 2

---
+++ snack2/index.php
@@ -0,0 +1,30 @@
+<?php
+$message = "";
+
+  if(!empty($_GET["name"]) || !empty($_GET["email"]) || !empty($_GET["age"]) ) {
+    $user_name = $_GET["name"];
+    $user_mail = $_GET["email"];
+    $user_age = $_GET["age"];
+
+    if(strlen($user_name) > 3 && strpos($user_mail,".") !== false && strpos($user_mail,"@") !== false && is_numeric($user_age)){
+      $message = "Hai accesso";
+    } else {
+      $message = "non hai accesso";
+    } else {
+    $message = "Inserisci tutti i campi obbligatori";
+    }
+  };
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Snack 2</title>
+  </head>
+  <body>
+
+    <p> <?php echo($message) ?> </p>
+
+  </body>
+</html>
